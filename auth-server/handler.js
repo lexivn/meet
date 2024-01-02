15be7bb4929360ff2dd9d83cb5f48fddc1ac894b
Alecks <52960889+lexivn@users.noreply.github.com>
2024-01-02 16:58:09 -0500

    Implemented the Serverless Functions
    Implemented the getAuth, getAccessToekn and getCalendarEvents serverless functions to the AWS platform.

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -15,16 +15,12 @@ const oAuth2Client = new google.auth.OAuth2(
   redirect_uris[0]
 );
 
-// Was created and exported using Node.js module.exports
 module.exports.getAuthURL = async () => {
   /**
    *
    * Scopes array is passed to the `scope` option. 
    *
    */
-
-  // To generate an authentication URL from Google API using
-  // the instance stored in oAuth2Client
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
@@ -40,4 +36,82 @@ module.exports.getAuthURL = async () => {
       authUrl,
     }),
   };
+};
+
+module.exports.getAccessToken = async (event) => {
+   // Decode authorization code extracted from the URL query
+   const code = decodeURIComponent(`${event.pathParameters.code}`);   
+   return new Promise((resolve, reject) => {
+    /**
+     *  Exchange authorization code for access token with a “callback” after the exchange,
+     *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
+     */
+    oAuth2Client.getToken(code, (error, response) => {
+      if (error) {
+        
+        return reject(error);        
+      }
+      return resolve(response);
+    });
+   })
+    .then((results) => {      
+      // Respond with OAuth token
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {      
+      // Handle error
+      return {        
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+};
+
+module.exports.getCalendarEvents = async (event) => {
+  // Decode authorization token extracted from the URL query
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+  oAuth2Client.setCredentials({access_token});
+
+  return new Promise((resolve, reject) => {
+    calendar.events.list(
+      {
+        calendarId: CALENDAR_ID,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );    
+  })
+   .then((results) => {           
+     return {
+       statusCode: 200,
+       headers: {
+         'Access-Control-Allow-Origin': '*',
+         'Access-Control-Allow-Credentials': true,
+       },
+       body: JSON.stringify({events:results.data.items}),
+     };
+   })
+   .catch((error) => {      
+     // Handle error
+     return {        
+       statusCode: 500,
+       body: JSON.stringify(error),
+     };
+   });
 };
