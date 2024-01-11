1d98845ec0c65e4e3c7ed71fac8d8c55479701f7
Alecks <52960889+lexivn@users.noreply.github.com>
2024-01-10 22:18:18 -0500

    Implemented the TDD for the App main page.
    Based on TDD. Verified the MeetUp should render:
    - A list of events.
    - Render a CitySearch
    - Render a Number of Event textbox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
+import CitySearch from './components/CitySearch';
+import EventList from './components/EventList';
 import './App.css';
+import NumberOfEvents from './components/NumberOfEvents';
 
 function App() {
+  const [noe, setNoe] = useState('32');
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div id="event-list" ></div> */}
+      <CitySearch />
+      <EventList />
+      <NumberOfEvents setNumberOfEvents={setNoe}/>      
     </div>
   );
 }
