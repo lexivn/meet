5be2d69275ee6c138829d494d0ef4212c302cf80
Alecks <52960889+lexivn@users.noreply.github.com>
2024-01-15 07:34:48 -0500

    Developed the Integration Testings
    Developed the Integration Testings

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React, { useState } from 'react';
-// import logo from './logo.svg';
-import CitySearch from './components/CitySearch';
-import EventList from './components/EventList';
-import './App.css';
-import NumberOfEvents from './components/NumberOfEvents';
+import React, { useEffect, useState } from "react";
+import CitySearch from "./components/CitySearch";
+import EventList from "./components/EventList";
+import NumberOfEvents from "./components/NumberOfEvents";
+import { extractLocations, getEvents } from "./api";
+import "./App.css";
 
 function App() {
   const [events, setEvents] = useState([]);
   const [currentNOE, setCurrentNOE] = useState(32);
-  
+  const [allLocaltions, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
+
+  useEffect(() => {
+    fetchData();
+  }, [currentCity]);
+
+  // const fetchData = async () => {
+  //   const allEvents = await getEvents();
+  //   setEvents(allEvents.slice(0, currentNOE)); // Getting all the events
+  //   setAllLocations(extractLocations(allEvents)); // Getting all the Locations
+  // }
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents = currentCity === "See all cities" ?
+      allEvents :
+      allEvents.filter(event => event.location === currentCity)
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  }
+
   return (
     <div className="App">
-      {/* <div id="event-list" ></div> */}
-      <CitySearch />
-      <EventList />
-      <NumberOfEvents setNumberOfEvents={setCurrentNOE}/>      
+      <CitySearch allLocations={allLocaltions} setCurrentCity={setCurrentCity} />
+      <NumberOfEvents setNumberOfEvents={setCurrentNOE} />
+      <EventList events={events} />
     </div>
   );
 }
